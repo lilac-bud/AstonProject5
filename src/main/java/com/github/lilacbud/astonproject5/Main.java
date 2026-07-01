df19907cb3cd9f2077cbb674f45d65f5ef53f6a7
RAX7 <antoxa2013@gmail.com>
2026-07-01 22:50:02 +0700

    Revert "test"
    This reverts commit d1e93163f8391ace268e4ce847790c641567717d.

--- src/main/java/com/github/lilacbud/astonproject5/Main.java
+++ src/main/java/com/github/lilacbud/astonproject5/Main.java
@@ -2,8 +2,6 @@ package com.github.lilacbud.astonproject5;
 
 import com.github.lilacbud.astonproject5.app.App;
 
-// Test
-
 public class Main {
     public static void main(String[] args) {
         var app = App.getInstance();
