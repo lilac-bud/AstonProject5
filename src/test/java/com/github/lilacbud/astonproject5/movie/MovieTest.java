893e04a7992d9df24d8eaae93e07b99275440feb
Veronika Belova <belovaveronika14@gmail.com>
2026-07-02 14:48:13 +0300

    проведение тестов

---
+++ src/test/java/com/github/lilacbud/astonproject5/movie/MovieTest.java
@@ -0,0 +1,25 @@
+package com.github.lilacbud.astonproject5.movie;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovieTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getYearOfRelease() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHourLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
