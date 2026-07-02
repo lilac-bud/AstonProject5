42a9d4394cce96d18bf537fe8c97b5474fd332ad
Veronika Belova <43645862+Avocoders@users.noreply.github.com>
2026-07-02 10:22:15 +0300

    Create Movie.java
    Реализация класса Movie, включая вложенный класс Builder

---
+++ src/main/java/com/github/lilacbud/astonproject5/Movie.java
@@ -0,0 +1,50 @@
+package com.github.lilacbud.astonproject5;
+
+import java.util.Comparator;
+
+public class Movie {
+    private final String name;
+    private final int yearOfRelease;
+    private final float hourLength;
+    
+    public static final Comparator<Movie> compareByName = Comparator.comparing(Movie::getName);
+    public static final Comparator<Movie> compareByYearOfRelease = Comparator.comparing(Movie::getYearOfRelease);
+    public static final Comparator<Movie> compareByHourLength = Comparator.comparingDouble(Movie::getHourLength);
+    
+    public String getName() { return name; }
+    public int getYearOfRelease() { return yearOfRelease; }
+    public float getHourLength() { return hourLength; }
+    
+    @Override
+    public String toString() {
+        return String.format("Movie {название=%-35s год=%d длительность=%.1f ч }", name, yearOfRelease, hourLength);
+    }
+    
+    public static class Builder{
+        String name;
+        int yearOfRelease;
+        float hourLength;
+        
+        public Builder withName(String name){
+            this.name=name;
+            return this;
+        }
+        public Builder withYearOfRelease(int year){
+            this.yearOfRelease=year;
+            return this;
+        }
+        public Builder withHourLength(float hourLength){
+            this.hourLength=hourLength;
+            return this;
+        }
+        public Movie build(){
+            return new Movie(this);
+        }
+    }
+    
+    Movie(Builder builder){
+        this.name= builder.name;
+        this.yearOfRelease=builder.yearOfRelease;
+        this.hourLength= builder.hourLength;
+    }
+}
