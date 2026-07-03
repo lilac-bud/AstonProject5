a0f450ec4fb2df8eab05b8b1df1ebe34ef1467c4
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-03 20:42:26 +0300

    Declare fillers' constructors

--- src/main/java/com/github/lilacbud/astonproject5/movie/RandomFiller.java
+++ src/main/java/com/github/lilacbud/astonproject5/movie/RandomFiller.java
@@ -3,6 +3,9 @@ package com.github.lilacbud.astonproject5.movie;
 import java.util.Collection;
 
 public class RandomFiller implements MoviesFiller{
+    public RandomFiller(int size){
+        
+    }
     @Override
     public void fillMovies(Collection<Movie> movies){
         
