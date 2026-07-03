45925bff0ce5d21f47f79cf7efb3a9f3a7afac4f
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-03 21:33:21 +0300

    Create save package
    Package 'save' includes interface MoviesSaver and its concrete implementation DefaultSaver, which is only declared.

---
+++ src/main/java/com/github/lilacbud/astonproject5/movie/save/DefaultSaver.java
@@ -0,0 +1,14 @@
+package com.github.lilacbud.astonproject5.movie.save;
+
+import com.github.lilacbud.astonproject5.movie.Movie;
+import java.util.Collection;
+
+public class DefaultSaver implements MoviesSaver{
+    public DefaultSaver(String filepath){
+        
+    }
+    @Override
+    public void save(Collection<Movie> movies){
+        
+    }
+}
