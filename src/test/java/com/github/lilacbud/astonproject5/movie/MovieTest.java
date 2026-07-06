d6c3edf80775eea8051e81e646294f21dac4beb3
Veronika Belova <belovaveronika14@gmail.com>
2026-07-06 17:18:41 +0300

    add dependensies in pom and rewrite movieTest

--- src/test/java/com/github/lilacbud/astonproject5/movie/MovieTest.java
+++ src/test/java/com/github/lilacbud/astonproject5/movie/MovieTest.java
@@ -1,9 +1,13 @@
 package com.github.lilacbud.astonproject5.movie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MovieTest {
     private Movie movie;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
         movie=new Movie.Builder()
                 .withName("Криминальное чтиво")
@@ -12,22 +16,22 @@ class MovieTest {
                 .build();
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getName() {
         assertEquals("Криминальное чтиво", movie.getName());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getYearOfRelease() {
         assertEquals(1994, movie.getYearOfRelease());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getHourLength() {
         assertEquals(2.5f, movie.getHourLength());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testToString() {
         String expected = String.format("Movie {название=%-35s год=%d длительность=%.1f ч }", "Криминальное чтиво", 1994, 2.5f);
         assertEquals(expected, movie.toString());
