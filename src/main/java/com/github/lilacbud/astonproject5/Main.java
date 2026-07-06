d83b0d2baafad76c9eb176d3af88362109dabcfe
Veronika Belova <belovaveronika14@gmail.com>
2026-07-06 16:12:58 +0300

    rewrite on private pool for builder

--- src/main/java/com/github/lilacbud/astonproject5/Main.java
+++ src/main/java/com/github/lilacbud/astonproject5/Main.java
@@ -8,7 +8,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Menu().run();
+        Menu.getInstance().run();
     }
     
 }
