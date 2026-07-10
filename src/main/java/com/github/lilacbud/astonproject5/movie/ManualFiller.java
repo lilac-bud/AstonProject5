51ac1ee584f0585eb0dcad951d9e745069b748a6
kondrashovaju <kondrashovaju@mail.ru>
2026-07-10 17:39:34 +0600

    Add Stream

--- src/main/java/com/github/lilacbud/astonproject5/movie/ManualFiller.java
+++ src/main/java/com/github/lilacbud/astonproject5/movie/ManualFiller.java
@@ -3,60 +3,66 @@ package com.github.lilacbud.astonproject5.movie;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
-public class ManualFiller implements MoviesFiller{
+public class ManualFiller implements MoviesFiller {
     private final int size;
+    static Scanner scanner = new Scanner(System.in);
 
-    public ManualFiller(int size){
+    public ManualFiller(int size) {
         this.size = size;
     }
 
     @Override
-    public void fillMovies(Collection<Movie> movies){
+    public void fillMovies(Collection<Movie> movies) {
         System.out.println("ЗАПОЛНЕНИЕ СПИСКА ФИЛЬМОВ ВРУЧНУЮ");
-        Scanner scanner = new Scanner(System.in);
 
-        int i = 1;
-        while (i <= size) {
-            System.out.print(String.format("Введите название фильма №%d: ", i));
-            String name = scanner.nextLine();
-            if (MovieInputValidation.validateName(name).isEmpty()) //если не прошли валидацию, пропускаем ввод этого фильма
-                continue;
+        IntStream.range(0, size) //создаем поток чисел от 0 до size-1
+                 .mapToObj(i -> ManualFiller.fillMovie()) //заменяем каждое число на результат вызова метода
+                 .forEach(movies::add); //добавляем фильм в коллекцию
 
-            System.out.print("Введите год выпуска: ");
-            int yearOfRelease;
-            String yearStr = scanner.nextLine();
-            Optional<Integer> verifiedYear = MovieInputValidation.validateYearOfRelease(yearStr);
-            if (verifiedYear.isEmpty())
-                continue;
-            else
-                yearOfRelease = verifiedYear.get();
+        //System.out.println("\nСПИСОК ДОБАВЛЕННЫХ ФИЛЬМОВ");
+        //movies.stream().forEach(System.out::println);
+    }
 
-            System.out.print("Введите продолжительность фильма: ");
-            float hourLength;
-            String hourStr = scanner.nextLine();
-            Optional<Float> verifiedHour = MovieInputValidation.validateHourLength(hourStr);
-            if (verifiedHour.isEmpty())
-                continue;
-            else
-                hourLength = verifiedHour.get();
-
-            Movie.Builder builder = new Movie.Builder();
-            Movie movie = builder.withName(name)
-                    .withYearOfRelease(yearOfRelease)
-                    .withHourLength(hourLength)
-                    .build();
-
-            movies.add(movie);
-
-            System.out.println("Фильм успешно добавлен");
-            i++;
+    //получаем данные о фильме
+    private static Movie fillMovie() {
+        //получаем название фильма
+        String name;
+        do {
+            System.out.print("Введите название фильма: ");
+            name = scanner.nextLine();
         }
+        while (MovieInputValidation.validateName(name).isEmpty()); //пока не будет введено корректное название
 
-        scanner.close();
+        //получаем год выпуска
+        int yearOfRelease; //год выпуска
+        String yearStr; //год выпуска в строковом представлении
+        Optional<Integer> verifiedYear; //результат метода валидации
+        do {
+            System.out.print("Введите год выпуска: ");
+            yearStr = scanner.nextLine();
+            verifiedYear = MovieInputValidation.validateYearOfRelease(yearStr);
+        }
+        while (verifiedYear.isEmpty());
+        yearOfRelease = verifiedYear.get();
 
-        System.out.println("\nСПИСОК ДОБАВЛЕННЫХ ФИЛЬМОВ");
+        //получаем продолжительность
+        float hourLength; //продолжительность
+        String hourStr; //продолжительность в строковом представлении
+        Optional<Float> verifiedHour; //результат метода валидации
+        do {
+            System.out.print("Введите продолжительность фильма: ");
+            hourStr = scanner.nextLine();
+            verifiedHour = MovieInputValidation.validateHourLength(hourStr);
+        }
+        while (verifiedHour.isEmpty());
+        hourLength = verifiedHour.get();
 
-        movies.stream().forEach(System.out::println);
+        //создаем объект
+        return new Movie.Builder().withName(name)
+                .withYearOfRelease(yearOfRelease)
+                .withHourLength(hourLength)
+                .build();
     }
 }
