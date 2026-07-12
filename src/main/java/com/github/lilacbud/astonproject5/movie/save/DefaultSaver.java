4b863efe932c400a33dc1eeaf536a3cde02598ca
Veronika Belova <belovaveronika14@gmail.com>
2026-07-12 16:25:00 +0300

    rewrite translate the message into Russian

--- src/main/java/com/github/lilacbud/astonproject5/movie/save/DefaultSaver.java
+++ src/main/java/com/github/lilacbud/astonproject5/movie/save/DefaultSaver.java
@@ -24,7 +24,7 @@ public class DefaultSaver implements MoviesSaver{
 
         if (Files.exists(filePath)){
             while(true){
-                System.out.println("The file already exists. 1 - overwrite, 2 - add");
+                System.out.println("Файл уже существует. 1 - перезаписать, 2 - добавить");
 
                 String answer = scanner.nextLine();
                 if(answer.equals("1")){
