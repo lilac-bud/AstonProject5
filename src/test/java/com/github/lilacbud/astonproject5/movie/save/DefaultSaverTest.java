c40e37763a1a11eab2203866640f145bc91d52a5
Veronika Belova <belovaveronika14@gmail.com>
2026-07-12 15:22:47 +0300

    create test class

---
+++ src/test/java/com/github/lilacbud/astonproject5/movie/save/DefaultSaverTest.java
@@ -0,0 +1,16 @@
+package com.github.lilacbud.astonproject5.movie.save;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultSaverTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void testSave() {
+    }
+}
