3d78cddb700ea68c4e86969c8b5f78b834ee6038
Veronika Belova <belovaveronika14@gmail.com>
2026-07-15 15:31:38 +0300

    refactor move method in util package in new class MovieCounter

--- src/main/java/com/github/lilacbud/astonproject5/App.java
+++ src/main/java/com/github/lilacbud/astonproject5/App.java
@@ -15,7 +15,6 @@ import static java.util.Objects.requireNonNull;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
---
+++ src/main/java/com/github/lilacbud/astonproject5/util/MovieCounter.java
@@ -0,0 +1,7 @@
+package com.github.lilacbud.astonproject5.util;
+
+public final class MovieCounter {
+    private MovieCounter(){
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+}
