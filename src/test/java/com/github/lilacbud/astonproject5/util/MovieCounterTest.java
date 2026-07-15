e833b8e06992e1923a6a01df93877104ea996ca2
Veronika Belova <belovaveronika14@gmail.com>
2026-07-15 15:47:55 +0300

    add test for MovieCounter

---
+++ src/test/java/com/github/lilacbud/astonproject5/util/MovieCounterTest.java
@@ -0,0 +1,12 @@
+package com.github.lilacbud.astonproject5.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieCounterTest {
+
+    @Test
+    void countInsert() {
+    }
+}
