c7b6f21c3d930bc6bbeb4f9dceaff04c2638846c
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-18 20:51:25 +0300

    Reduce copying in merge sort

--- src/main/java/com/github/lilacbud/astonproject5/movie/sort/MergeSort.java
+++ src/main/java/com/github/lilacbud/astonproject5/movie/sort/MergeSort.java
@@ -3,7 +3,6 @@ package com.github.lilacbud.astonproject5.movie.sort;
 import com.github.lilacbud.astonproject5.movie.Movie;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import static java.util.Objects.requireNonNull;
@@ -12,37 +11,28 @@ public class MergeSort implements SortingStrategy {
 
     @Override
     public void sort(List<Movie> movies, Comparator<Movie> comp) {
-        if (requireNonNull(movies, "Movies must not be null").isEmpty()) {
+        if (requireNonNull(movies, "Movies must not be null").size() <= 1) {
             return;
         }
-
-        List<Movie> listMovies = new ArrayList<>(movies);
-        List<Movie> result = doMergeSort(listMovies, comp);
-
+        final List<Movie> result = doMergeSort(movies, comp);
         movies.clear();
         movies.addAll(result);
     }
 
     private List<Movie> doMergeSort(List<Movie> movies, Comparator<Movie> comp) {
-        if(movies.size() <= 1) {
+        final int size = movies.size();
+        if (size <= 1) {
             return movies;
         }
-
-        int moviesSize = movies.size();
-        int middle = moviesSize / 2;
-
-        List<Movie> left = new ArrayList<>(movies.subList(0, middle));
-        List<Movie> right = new ArrayList<>(movies.subList(middle, moviesSize));
-
-        left = doMergeSort(left, comp);
-        right = doMergeSort(right, comp);
-
+        final int middle = size / 2;
+        final List<Movie> left = doMergeSort(movies.subList(0, middle), comp);
+        final List<Movie> right = doMergeSort(movies.subList(middle, size), comp);
         return merge(left, right, comp);
     }
 
     private List<Movie> merge(List<Movie> left, List<Movie> right, Comparator<Movie> comp) {
-        List<Movie> result = new LinkedList<>();
-        ListIterator<Movie> leftIt = left.listIterator(), rightIt = right.listIterator();
+        final List<Movie> result = new ArrayList<>();
+        final ListIterator<Movie> leftIt = left.listIterator(), rightIt = right.listIterator();
         while (rightIt.hasNext()) {
             var rightElem = rightIt.next();
             while (leftIt.hasNext()) {
