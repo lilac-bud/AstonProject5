3a7ffc947e44c8f5cb25d55d098abf4b9f0c6dbb
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-19 19:49:15 +0300

    Make methods in RandomFiller more generic

--- src/main/java/com/github/lilacbud/astonproject5/movie/RandomFiller.java
+++ src/main/java/com/github/lilacbud/astonproject5/movie/RandomFiller.java
@@ -2,6 +2,7 @@ package com.github.lilacbud.astonproject5.movie;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Base64;
 import java.util.Collection;
 import static java.util.Objects.requireNonNull;
 import java.util.Random;
@@ -11,11 +12,10 @@ public class RandomFiller implements MoviesFiller {
     private static final Random RANDOM = new Random();
     private static final int MIN_NAME_LENGTH = 5;
     private static final int MAX_NAME_LENGTH = 10;
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz"; 
     private static final int MAX_YEAR = 2030;
     private static final float MAX_HOUR_LENGTH = 3F;
     private static final int HL_PRECISION = 3;
-    private static final float HL_EPS = (float)(1F / Math.pow(10, 3));
+    private static final float HL_EPS = 1F / (float)Math.pow(10, 3);
     
     private final int size;
     private final Movie.Builder builder = new Movie.Builder();
@@ -38,35 +38,47 @@ public class RandomFiller implements MoviesFiller {
     private Movie generateMovie(){
         return builder
                 .withName(generateName())
-                .withYearOfRelease(generateYear())
+                .withYearOfRelease(generateYearOfRelease())
                 .withHourLength(generateHourLength())
                 .build();
     }
     
-    private String generateName() {
-        final int length = RANDOM.nextInt(MIN_NAME_LENGTH, MAX_NAME_LENGTH + 1);
-        final char[] temp = new char[length];
-        for (int i = 0; i < length; ++i) {
-            temp[i] = CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length()));
-        }
-        return new String(temp);
+    private String generateName() { 
+        return generateString(MIN_NAME_LENGTH, MAX_NAME_LENGTH + 1);
     }
     
-    private int generateYear() {
-        return RANDOM.nextInt(MovieInputValidation.MIN_YEAR, MAX_YEAR + 1);
+    private int generateYearOfRelease() {
+        return generateInt(MovieInputValidation.MIN_YEAR, MAX_YEAR +1);
     }
     
     private float generateHourLength() {
-        final float hourLength = RANDOM.nextFloat(MovieInputValidation.MIN_HOUR_LENGTH, MAX_HOUR_LENGTH + HL_EPS);
-        return round(hourLength);
+        return generateFloatWithPrecision(MovieInputValidation.MIN_HOUR_LENGTH, MAX_HOUR_LENGTH + HL_EPS, HL_PRECISION);
     }
     
-    private float round(float hourLength) {
-        if (HL_PRECISION < 0) {
-            return hourLength;
+    private String generateString(int length) {
+        final byte[] temp = new byte[length];
+        RANDOM.nextBytes(temp);
+        return Base64.getUrlEncoder().encodeToString(temp);
+    }
+    
+    private String generateString(int minLengthIncl, int maxLengthExcl) {
+        return generateString(generateInt(minLengthIncl, maxLengthExcl));
+    }
+    
+    private int generateInt(int minIncl, int maxExcl) {
+        return RANDOM.nextInt(minIncl, maxExcl);
+    }
+    
+    private float generateFloat(float minIncl, float maxExcl) {
+        return RANDOM.nextFloat(minIncl, maxExcl);
+    }
+    
+    private float generateFloatWithPrecision(float minIncl, float maxExcl, int precision) {
+        if (precision < 0) {
+            throw new IllegalArgumentException("Precision must not be negative");
         }
-        BigDecimal temp = new BigDecimal(Float.toString(hourLength));
-        temp = temp.setScale(HL_PRECISION, RoundingMode.HALF_UP);
+        BigDecimal temp = new BigDecimal(Float.toString(generateFloat(minIncl, maxExcl)));
+        temp = temp.setScale(precision, RoundingMode.HALF_UP);
         return temp.floatValue();
     }
 }
