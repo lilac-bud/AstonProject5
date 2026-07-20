d7120fa88e823cda792a1042df1f1e484ec40038
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-20 22:14:20 +0300

    Update InputRequestTest

--- src/test/java/com/github/lilacbud/astonproject5/util/InputRequestTest.java
+++ src/test/java/com/github/lilacbud/astonproject5/util/InputRequestTest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.Scanner;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.anyString;
 import org.mockito.MockedStatic;
@@ -15,23 +16,27 @@ public class InputRequestTest {
     private final PrintStream originalOut = System.out;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+    
     @AfterEach
     public void tearDown() {
         System.setOut(originalOut);
     }
 
     @Test
-    public void testAskStringGivenNullScanner() {
-        System.out.println("askString given null scanner");
-        var thrown = assertThrows(NullPointerException.class, () -> InputRequest.askString(null));
+    public void givenNullAsScanner_whenAskingForString_thenThrow() {
+        final var thrown = assertThrows(NullPointerException.class, () -> InputRequest.askString(null));
         assertEquals(InputRequest.SCANNER_NULL_MESSAGE, thrown.getMessage());
     }
     
     @Test
-    public void testAskStringGivenValidScanner() {
-        System.out.println("askString given valid scanner");
-        System.setOut(new PrintStream(outContent));
+    public void givenScanner_whenAskingForString_thenReturnString() {
         final String prompt = "Filepath:";
+        final String expectedResult = "filepath";
+        final String input = String.format("\n \n%s\n", expectedResult);
         String result;
         try (MockedStatic<InputValidation> validation = mockStatic(InputValidation.class)) {
             validation.when(() -> InputValidation.validateInput(anyString()))
@@ -39,24 +44,23 @@ public class InputRequestTest {
             validation.when(() -> InputValidation.validateInput("")).thenReturn(Optional.empty());
             validation.when(() -> InputValidation.validateInput(" ")).thenReturn(Optional.empty());
 
-            result = InputRequest.askString(new Scanner("\n \nfilepath\n"), prompt);
+            result = InputRequest.askString(new Scanner(input), prompt);
         }
-        assertEquals("filepath", result);
+        assertEquals(expectedResult, result);
         assertEquals(prompt.repeat(3), outContent.toString().trim());
     }
 
     @Test
-    public void testAskIntegerGivenNullScanner() {
-        System.out.println("askInteger given null scanner");
-        var thrown = assertThrows(NullPointerException.class, () -> InputRequest.askInteger(null));
+    public void givenNullAsScanner_whenAskingForInteger_thenThrow() {
+        final var thrown = assertThrows(NullPointerException.class, () -> InputRequest.askInteger(null));
         assertEquals(InputRequest.SCANNER_NULL_MESSAGE, thrown.getMessage());
     }
     
     @Test
-    public void testAskIntegerGivenValidScanner() {
-        System.out.println("askInteger given valid scanner");
-        System.setOut(new PrintStream(outContent));
+    public void givenScanner_whenAskingForInteger_thenReturnInteger() {
         final String prompt = "Size:";
+        final int expectedResult = 2;
+        final String input = String.format("\n \nqwerty\n-2\n%d\n", expectedResult);
         int result;
         try (MockedStatic<InputValidation> validation = mockStatic(InputValidation.class)) {
             validation.when(() -> InputValidation.validateIntegerInput(anyString()))
@@ -66,9 +70,9 @@ public class InputRequestTest {
             validation.when(() -> InputValidation.validateIntegerInput("qwerty")).thenReturn(Optional.empty());
             validation.when(() -> InputValidation.validateIntegerInput("-2")).thenReturn(Optional.empty());
 
-            result = InputRequest.askInteger(new Scanner("\n \nqwerty\n-2\n2\n"), prompt);
+            result = InputRequest.askInteger(new Scanner(input), prompt);
         }
-        assertEquals(2, result);
+        assertEquals(expectedResult, result);
         assertEquals(prompt.repeat(5), outContent.toString().trim());
     }
 }
