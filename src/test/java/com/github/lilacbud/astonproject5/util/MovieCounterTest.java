d463b0d55c812d6c4eb5995adc71e8e74f363024
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-22 02:30:21 +0300

    Update MovieCounterTest

--- src/test/java/com/github/lilacbud/astonproject5/util/MovieCounterTest.java
+++ src/test/java/com/github/lilacbud/astonproject5/util/MovieCounterTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -14,35 +15,52 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class MovieCounterTest {
     @Mock
     private Movie movie;
-
+    
     @Test
-    public void testCountInsertNotFound() {
-        when(movie.getName()).thenReturn("Интерстеллар", "Начало", "Интерстеллар", "Криминальное чтиво");
-        System.out.println("countInsert given movies without target");
-        List<Movie> movies = Collections.nCopies(4, movie);
-        int result = MovieCounter.countInsert(movies, "Дюна");
-        assertEquals(0,result);
+    public void giveThatListHasTarget_whenCounting_thenReturnAmount() {
+        final List<String> names = List.of("Интерстеллар", "Начало", "Интерстеллар", "Криминальное чтиво");
+        when(movie.getName()).thenAnswer(AdditionalAnswers.returnsElementsOf(names));
+        final List<Movie> movies = Collections.nCopies(names.size(), movie);
+        final String target = "Интерстеллар";
+        final int expectedResult = 2;
+        final int result = MovieCounter.countInsert(movies, target);
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testCountInsertListIsEmpty(){
-        System.out.println("countInsert given empty movies");
-        int result = MovieCounter.countInsert(List.of(), "Интерстеллар");
-        assertEquals(0, result);
+    public void givenThatListHasNotTarget_whenCounting_thenReturnZero() {
+        final List<String> names = List.of("Интерстеллар", "Начало", "Интерстеллар", "Криминальное чтиво");
+        when(movie.getName()).thenAnswer(AdditionalAnswers.returnsElementsOf(names));
+        final List<Movie> movies = Collections.nCopies(names.size(), movie);
+        final String target = "Дюна";
+        final int expectedResult = 0;
+        final int result = MovieCounter.countInsert(movies, target);
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testCountInsertTargetIsNull(){
-        System.out.println("countInsert given null target");
-        List<Movie> movies = Collections.nCopies(4, movie);
-        int result = MovieCounter.countInsert(movies, null);
-        assertEquals(0, result);
+    public void givenThatListIsEmpty_whenCounting_thenReturnZero(){
+        final String target = "Интерстеллар";
+        final int expectedResult = 0;
+        int result = MovieCounter.countInsert(List.of(), target);
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testCountInsertListIsNull(){
-        System.out.println("countInsert given null movies");
-        int result = MovieCounter.countInsert(null, "Интерстеллар");
-        assertEquals(0,result);
+    public void givenNullAsTarget_whenCounting_thenReturnZero(){
+        final String target = null;
+        final List<Movie> movies = Collections.nCopies(4, movie);
+        final int expectedResult = 0;
+        int result = MovieCounter.countInsert(movies, target);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void givenNullAsList_whenCounting_thenReturnZero(){
+        final String target = "Интерстеллар";
+        final List<Movie> movies = null;
+        final int expectedResult = 0;
+        int result = MovieCounter.countInsert(movies, target);
+        assertEquals(expectedResult, result);
     }
 }
