6280be018d70793f7d451994643ae234c9a04f3b
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-07-22 01:06:30 +0300

    Update EvenNumbersSortDecoratorTest

--- src/test/java/com/github/lilacbud/astonproject5/movie/sort/EvenNumbersSortDecoratorTest.java
+++ src/test/java/com/github/lilacbud/astonproject5/movie/sort/EvenNumbersSortDecoratorTest.java
@@ -1,133 +1,67 @@
 package com.github.lilacbud.astonproject5.movie.sort;
 
-import com.github.lilacbud.astonproject5.movie.Movie;
 import com.github.lilacbud.astonproject5.sort.EvenNumbersSortDecorator;
 import com.github.lilacbud.astonproject5.sort.MergeSort;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import static org.mockito.Mockito.when;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class EvenNumbersSortDecoratorTest {
-
-    private List<Movie> movies;
-
-    @Mock
-    private Movie movie1, movie2, movie3, movie4, movie5, movie6;
-
-    private void configureOddYearMovieMocks() {
-        when(movie2.getYearOfRelease()).thenReturn(2011);
-        when(movie5.getYearOfRelease()).thenReturn(2001);
-        when(movie6.getYearOfRelease()).thenReturn(1999);
-    }
-    
-    private void configureEvenYearMovieMocks() {
-        when(movie1.getYearOfRelease()).thenReturn(2010);
-        when(movie3.getYearOfRelease()).thenReturn(1994);
-        when(movie4.getYearOfRelease()).thenReturn(2008);
-    }
-    
-    @BeforeEach
-    public void setUp() {
-        movies = new ArrayList<>(List.of(movie1, movie2, movie3,movie4,movie5,movie6));
-    }
+    private final EvenNumbersSortDecorator<Integer> sortingStrategy = 
+            new EvenNumbersSortDecorator<>(new MergeSort<>(), i -> i);
     
     @Test
-    void testCreateEvenNumbersSortDecoratorWithNullSortStrategy() {
-        System.out.println("EvenNumbersSortDecorator with null sort strategy");
-        var thrown = assertThrows(NullPointerException.class, () -> 
-                new EvenNumbersSortDecorator<Movie>(null, Movie::getYearOfRelease));
+    public void givenNullAsSortStrategy_whenCreatingDecorator_thenThrow() {
+        final var thrown = assertThrows(NullPointerException.class, () -> 
+                new EvenNumbersSortDecorator<Integer>(null, i -> i));
         assertEquals(EvenNumbersSortDecorator.SORTSTRAT_NULL_MESSAGE, thrown.getMessage());
     }
     
     @Test
-    void createEvenNumbersSortDecoratorWithNullExtractor() {
-        System.out.println("EvenNumbersSortDecorator with null extractor");
-        var thrown = assertThrows(NullPointerException.class, () -> 
-                new EvenNumbersSortDecorator<Movie>(new MergeSort<>(), null));
+    public void givenNullAsExtractor_whenCreatingDecorator_thenThrow() {
+        final var thrown = assertThrows(NullPointerException.class, () -> 
+                new EvenNumbersSortDecorator<Integer>(new MergeSort<>(), null));
         assertEquals(EvenNumbersSortDecorator.EXTRACTOR_NULL_MESSAGE, thrown.getMessage());
     }
 
     @Test
-    void evenNumbersSortCorrectTest() {
-        configureOddYearMovieMocks();
-        configureEvenYearMovieMocks();
-        System.out.println("sort");
-        Comparator<Movie> comparator = Comparator.comparing(Movie::getYearOfRelease);
-
-        EvenNumbersSortDecorator<Movie> sortingStrategy = 
-                new EvenNumbersSortDecorator<>(new MergeSort<>(),Movie::getYearOfRelease);
-        sortingStrategy.sort(movies, comparator);
-
-        assertEquals(6, movies.size());
-        assertEquals(List.of(movie3,movie2,movie4,movie1,movie5,movie6), movies);
+    public void givenNaturalOrderAsComparator_whenSortingIntegerList_thenSortOnlyEvenNumbers() {
+        final List<Integer> list = new ArrayList<>(List.of(6, 3, 2, 1, 5, 4));
+        final List<Integer> expectedList = List.of(2, 3, 4, 1, 5, 6);
+        sortingStrategy.sort(list, Comparator.naturalOrder());
+        assertEquals(expectedList, list);
     }
 
     @Test
-    void onlyEvenEvenNumbersSortCorrectTest() {
-        configureEvenYearMovieMocks();
-        System.out.println("sort given movies with even year");
-        movies.clear();
-        movies.addAll(List.of(movie1,movie3,movie4));
-
-        Comparator<Movie> comparator = Comparator.comparing(Movie::getYearOfRelease);
-
-        EvenNumbersSortDecorator<Movie> sortingStrategy = 
-                new EvenNumbersSortDecorator<>(new MergeSort<>(),Movie::getYearOfRelease);
-        sortingStrategy.sort(movies, comparator);
-
-        assertEquals(3, movies.size());
-        assertEquals(List.of(movie3,movie4,movie1), movies);
+    public void givenOnlyEvenList_whenSortingList_thenSort() {
+        final List<Integer> evenList = new ArrayList<>(List.of(6, 2, 4));
+        final List<Integer> expectedList = List.of(2, 4, 6);
+        sortingStrategy.sort(evenList, Comparator.naturalOrder());
+        assertEquals(expectedList, evenList);
     }
 
     @Test
-    void onlyOddEvenNumbersSortCorrectTest() {
-        configureOddYearMovieMocks();
-        System.out.println("sort given movies with odd year");
-        movies.clear();
-        movies.addAll(List.of(movie2,movie5,movie6));
-
-        Comparator<Movie> comparator = Comparator.comparing(Movie::getYearOfRelease);
-
-        EvenNumbersSortDecorator<Movie> sortingStrategy = 
-                new EvenNumbersSortDecorator<>(new MergeSort<>(),Movie::getYearOfRelease);
-        sortingStrategy.sort(movies, comparator);
-
-        assertEquals(3, movies.size());
-        assertEquals(List.of(movie2,movie5,movie6), movies);
+    public void givenOnlyOddList_whenSortingList_thenDoNotSort() {
+        final List<Integer> oddList = new ArrayList<>(List.of(3, 1, 5));
+        final List<Integer> expectedList = List.copyOf(oddList);
+        sortingStrategy.sort(oddList, Comparator.naturalOrder());
+        assertEquals(expectedList, oddList);
     }
 
     @Test
-    void evenNumbersEmptyCollectionTest() {
-        System.out.println("sort given empty movies");
-        movies.clear();
-        Comparator<Movie> comparator = Comparator.comparing(Movie::getYearOfRelease);
-
-        EvenNumbersSortDecorator<Movie> sortingStrategy = 
-                new EvenNumbersSortDecorator<>(new MergeSort<>(),Movie::getYearOfRelease);
-        sortingStrategy.sort(movies, comparator);
-
-        assertTrue(movies.isEmpty());
+    public void givenEmptyList_whenSortingList_thenThrowNothing() {
+        final List<Integer> list = new ArrayList<>();
+        assertDoesNotThrow(() -> sortingStrategy.sort(list, Comparator.naturalOrder()));
+        assertTrue(list.isEmpty());
     }
 
     @Test
-    void evenNumbersMoviesIsNullTest() {
-        System.out.println("sort given null movies");
-        movies = null;
-        Comparator<Movie> comparator = Comparator.comparing(Movie::getYearOfRelease);
-
-        EvenNumbersSortDecorator<Movie> sortingStrategy = 
-                new EvenNumbersSortDecorator<>(new MergeSort<>(),Movie::getYearOfRelease);
-        var thrown = assertThrows(NullPointerException.class, () -> sortingStrategy.sort(movies, comparator));
-
+    public void givenNullAsList_whenSortingList_thenThrow() {
+        final List<Integer> list = null;
+        final var thrown = assertThrows(NullPointerException.class, 
+                () -> sortingStrategy.sort(list, Comparator.naturalOrder()));
         assertEquals(EvenNumbersSortDecorator.LIST_NULL_MESSAGE, thrown.getMessage());
     }
-
 }
